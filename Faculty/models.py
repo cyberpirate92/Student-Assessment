3a866e8265b51567158817660be9520f3c25ab0e
Ravi Teja <bsravi.tez@gmail.com>
2016-02-26 02:06:29 +0530

    Defined a few models

---
+++ Faculty/models.py
@@ -0,0 +1,79 @@
+from django.db import models
+
+
+class FacultyLogin(models.Model):
+    username = models.CharField(max_length=20,primary_key=True)
+    password = models.CharField(max_length=255)
+
+
+# In natural language, A test 'test_id' with an optional name 'test_name' has been created by faculty 'faculty_id'
+
+class TestInfo(models.Model):
+    test_name = models.CharField(max_length=30,null=True)
+    test_id = models.IntegerField(primary_key=True)
+    faculty_id = models.ForeignKey(FacultyLogin,on_delete=models.CASCADE)
+
+
+class StudentLogin(models.Model):
+    username = models.CharField(max_length=20,primary_key=True)
+    password = models.CharField(max_length=255)
+
+
+class MCQQuestion(models.Model):
+    question = models.TextField()
+    choice1 = models.CharField(max_length=300)
+    choice2 = models.CharField(max_length=300)
+    choice3 = models.CharField(max_length=300)
+    choice4 = models.CharField(max_length=300)
+    correctAnswer = models.IntegerField(default=0)
+    test_id = models.ForeignKey(TestInfo,on_delete=models.CASCADE)
+
+
+class CodeQuestion(models.Model):
+    question = models.TextField()
+    testcase1_input = models.TextField()
+    testcase2_input = models.TextField()
+    testcase3_input = models.TextField()
+    testcase1_output = models.TextField()
+    testcase2_output = models.TextField()
+    testcase3_output = models.TextField()
+    test_id = models.ForeignKey(TestInfo,on_delete=models.CASCADE)
+
+# In the below model, the question id could also be declared to be a foreign key referencing the MCQQuestions Model,
+# but test_id is declared as a foreign key and is enough, because when test_id is removed,
+# so are all the question ids related to test_id
+# It is also important to note that the more the number of foreign keys, more the time a DELETE query takes
+#
+# In natural language, A student 'username' has selected the answer 'selected_ans' for the question 'question_id' in the
+# test 'test_id'
+
+class MCQTestAnswers(models.Model):
+    test_id = models.ForeignKey(TestInfo,on_delete=models.CASCADE)
+    username = models.ForeignKey(StudentLogin,on_delete=models.CASCADE)
+    selected_ans = models.IntegerField(default=1)
+    question_id = models.IntegerField(default=0)
+
+# The following model is used for identifying which which faculty is linked with what subjects
+# The faculty username is a foreign key from the FacultyLogin table, while the subject is just a string
+#
+# In natural language, A faculty 'username' has subject 'subject' in slot 'slot'
+
+class Faculty_Subjects(models.Model):
+    subject = models.CharField(max_length=10)
+    slot = models.CharField(max_length=15)
+    username = models.ForeignKey(FacultyLogin,on_delete=models.CASCADE)
+
+
+# The following model is used to assign students to subjects, please note that subject is just a string, not a
+# foreign key reference
+# .
+# In natural language, a student 'student_username' has registered under faculty 'faculty_username'
+# for subject 'subject'
+
+
+class Student_Subject(models.Model):
+    student_username = models.ForeignKey(StudentLogin,on_delete=models.CASCADE)
+    faculty_username = models.ForeignKey(FacultyLogin,on_delete=models.CASCADE)
+    subject = models.CharField(max_length=15)
+
+
