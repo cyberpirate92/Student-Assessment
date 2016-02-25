d87fdb98e55b556760077a944c60285571fbeb6e
Ravi Teja <bsravi.tez@gmail.com>
2016-02-26 02:42:20 +0530

    Registered Login models in admin.py

--- Faculty/admin.py
+++ Faculty/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import FacultyLogin,StudentLogin
 
 # Register your models here.
+
+admin.site.register(FacultyLogin)
+admin.site.register(StudentLogin)
